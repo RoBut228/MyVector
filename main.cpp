29f163f521f84718eacd8e9eb5aada837154513e
RoBut228 <roma.butrimov@gmail.com>
2019-12-06 22:27:06 +0300

    FINAL

--- main.cpp
+++ main.cpp
@@ -3,23 +3,59 @@
 #include <cmath>
 #include <windows.h>
 
+using vec_int = MyVector<int>;
+
+vec_int test ()
+{
+    vec_int a{1,2,3,4};
+    return a;
+}
+
+void test2 (vec_int f)
+{
+    print(f);
+}
+
 
 int main()
 {
-    size_t i = 0;
-    while(true)
-    {
-        MyVector<string> d(10000);
-        for (int j=0;j<10000;++j)
-        {
-            std::string s = std::string(1000, 't');
-            d[j]=s;
+      MyVector<int> t(10);
+      cout << "MyVector 't' default\n";
+      print(t);
 
-           cout<<d[j]<<endl;
-        }
-      //  ++i;
-    //    cout<<endl<<endl<<"Number of try : "<<i<<endl<<endl<<endl;
-     //   Sleep(1000);
-    }
+      MyVector<int> a{0, 1, 2, 3};
+      a.push_back(0);
+
+      cout << "MyVector 'a' initializer_list and push_back\n";
+      print(a);
+
+      MyVector<int> b{a};
+
+      MyVector<int> c;
+
+      cout << "MyVector 'b' copy constructor:\n";
+      print(b);
+
+       c = move(b);
+
+      cout << "MyVector 'c' values after move \n";
+      print(c);
+
+
+      vec_int te = test();
+      test2(std::move(te));
+
+      print(te);
+
+
+//      while (true)
+//      {
+//          MyVector<string> d;
+//          string s = string (10000,'q');
+//          for (size_t i = 0; i < 10000; ++i)
+//          {
+//              d.push_back(s);
+//          }
+//      }
 }
 
--- myvector.h
+++ myvector.h
@@ -7,42 +7,24 @@
 #include <iterator>
 using namespace std;
 
-//class Vector_base
+
 
 template <typename T, typename A>
 struct Vector_base
 {
+    using size_type = size_t;
     A alloc;
     T* elem;
-    size_t sz;
-    size_t space;
+    size_type sz;
+    size_type space;
     
-    Vector_base (A a, size_t n)
+    Vector_base (A a, size_type n)
         : alloc {a}, elem {a.allocate(n)}, sz {n}, space {n}{}
-
-    Vector_base(Vector_base&& arg);
-
-    Vector_base& operator=(Vector_base&& arg);
     
-    ~Vector_base() {alloc.deallocate(elem, space);}
+    ~Vector_base() {
+        alloc.deallocate(elem, space);
+    }
 };
-
-template <typename T, typename A>
-Vector_base<T,A>::Vector_base(Vector_base&& arg)
-{
-    arg.sz=0;
-    arg.elem=nullptr;
-    arg.space=0;
-}
-
-template <typename T, typename A>
-Vector_base<T,A>& Vector_base<T,A>::operator=(Vector_base&& arg)
-{
-    swap(this->elem, arg.elem);
-    swap(this->sz, arg.sz);
-    swap(this->space, arg.space);
-    return *this;
-}
 
 //end of class Vector_base
 
@@ -52,42 +34,44 @@ template<typename T, typename A = allocator<T>>
 class MyVector : private Vector_base<T,A>
 {
 public:
-    explicit MyVector(size_t n, T d = T(), A alloc = A());
+    using size_type = typename Vector_base<T,A>::size_type;
+
+    explicit MyVector(size_type n, T d = T(), A alloc = A());
     
     MyVector(initializer_list<T> lst, const A& alloc = A());
     
-    MyVector (const A& alloc = A()): Vector_base<T, A> (alloc,1){}
+    MyVector (const A& alloc = A()): Vector_base<T, A> (alloc,0){}
     
     MyVector (const MyVector& arg);
     MyVector& operator= (const MyVector& v);
     
-    MyVector (MyVector&& a) = default;
-    MyVector& operator= (MyVector&& a) = default;
+    MyVector (MyVector&& a);
+    MyVector& operator= (MyVector&& a);
     
     ~MyVector();
     
-    T& at (size_t n);
-    const T& at (size_t n) const;
+    T& at (size_type n);
+    const T& at (size_type n) const;
     
-    T& operator[] (size_t n);
-    const T& operator[] (size_t n) const;
+    T& operator[] (size_type n);
+    const T& operator[] (size_type n) const;
     
-    size_t size () const {return this->sz;}
+    size_type size () const {return this->sz;}
     
-    size_t capacity() const { return this->space;}
+    size_type capacity() const { return this->space;}
     
-    void reserve (size_t newalloc);
+    void reserve (size_type newalloc);
     
-    void resize (size_t& newsize, T val = T());
+    void resize (size_type& newsize, T val = T());
     
     void push_back (const T& d);
 };
 
 template <typename T, typename A>
-MyVector<T,A>::MyVector(size_t n, T d, A alloc)
+MyVector<T,A>::MyVector(size_type n, T d, A alloc)
     :Vector_base<T, A> (alloc, n)
 {
-    for (int i=0; i<n; ++i)
+    for (size_type i=0; i<n; ++i)
         this->alloc.construct(&this->elem[i], d);
 }
 
@@ -95,14 +79,14 @@ template <typename T, typename A>
 MyVector<T,A>::MyVector(initializer_list<T> lst, const A& alloc)
     :Vector_base<T, A> (alloc, lst.size())
 {
-    for (int i=0; i<lst.size(); ++i)
-        this->alloc.construct(&this->elem[i],*(lst.begin()+1));
+    for (size_type i=0; i<lst.size(); ++i)
+        this->alloc.construct(&this->elem[i],*(lst.begin()+i));
 }
 
 template <typename T, typename A>
 MyVector<T,A>::~MyVector()
 {
-    for (int i = 0; i<this->sz; ++i)
+    for (size_type i = 0; i<this->sz; ++i)
         this->alloc.destroy(this->elem+i);
 }
 
@@ -110,7 +94,7 @@ template <typename T, typename A>
 MyVector<T,A>::MyVector(const MyVector& arg)
     :Vector_base<T,A>(arg.alloc,arg.sz)
 {
-    for (int i=0; i<arg.sz; ++i)
+    for (size_type i=0; i<arg.sz; ++i)
         this->alloc.construct(&this->elem[i], arg[i]);
 }
 
@@ -119,77 +103,108 @@ MyVector<T, A>& MyVector<T, A>::operator=(const MyVector<T, A>& arg)
 {
     if (this == &arg)
         return *this;
-    Vector_base<T, A> b{ this->alloc, arg.sz };
-    uninitialized_copy(arg.begin(), arg.end(), b.elem);
+    Vector_base<T, A> b (this->alloc, arg.sz);
+    uninitialized_copy(&arg[0], &arg[arg.size()], b.elem);
     swap(this->elem, b.elem);
     swap(this->sz, b.sz);
     swap(this->space, b.space);
-    for (size_t i = 0; i < b.sz; ++i)
+    for (size_type i = 0; i < b.sz; ++i)
         b.alloc.destroy(b.elem + i);//free memory used for vector before
     return *this;
 }
 
 template <typename T, typename A>
-void MyVector<T,A>::reserve(size_t newalloc)
+MyVector<T,A>::MyVector(MyVector&& arg)
+    :Vector_base<T,A>(arg.alloc,arg.sz)
+{
+    arg.sz=0;
+    arg.elem=nullptr;
+    arg.space=0;
+    std::cout<<endl<<"yes"<<endl<<endl;
+}
+
+template <typename T, typename A>
+MyVector<T, A>& MyVector<T, A>::operator=(MyVector<T, A>&& arg)
+{
+    swap(this->elem, arg.elem);
+    swap(this->sz, arg.sz);
+    swap(this->space, arg.space);
+    delete[] arg.elem;
+    std::cout<<endl<<"yes"<<endl<<endl;
+    return *this;
+}
+
+template <typename T, typename A>
+void MyVector<T,A>::reserve(size_type newalloc)
 {
     if (newalloc <= this->space) return;
     Vector_base<T,A> b (this->alloc, newalloc);
-    uninitialized_copy (b.elem, &b.elem[this->sz], this->elem);
-    for (size_t i = 0; i < this->sz; ++i)
+    uninitialized_copy (&this->elem[0], &this->elem[this->sz], b.elem);
+    for (size_type i = 0; i < this->sz; ++i)
     {
         this->alloc.destroy (&this->elem[i]);
     }
-    swap (this->sz,b.sz);
-    swap (this->space,b.space);
-    swap (this->alloc,b.alloc);
-    swap (this->elem,b.elem);
+    b.sz = this->sz;
+    swap(this->elem, b.elem);
+    swap(this->sz, b.sz);
+    swap(this->space, b.space);
 }
 
 template <typename T, typename A>
 void MyVector<T,A>::push_back (const T& val)
 {
-    if (!this->space)
-    reserve(8);
+    if (!(this->space))
+        reserve(8);
     else if (this->sz == this->space)
-    reserve(2 * this->space);
+        reserve(2 * this->space);
     this->alloc.construct(&this->elem[this->sz], val);
     ++this->sz;
 }
 
 template <typename T, typename A>
-void MyVector<T,A>::resize (size_t& newsize, T val)
+void MyVector<T,A>::resize (size_type& newsize, T val)
 {
     reserve (newsize);
-    for (size_t i = this->sz; i < newsize; ++i) this->alloc.construct (&this->elem[i], val);
-    for (size_t i = newsize; i < this->sz; ++i) this->alloc.destroy (&this->elem[i]);
+    for (size_type i = this->sz; i < newsize; ++i) this->alloc.construct (&this->elem[i], val);
+    for (size_type i = newsize; i < this->sz; ++i) this->alloc.destroy (&this->elem[i]);
     this->sz = newsize;
 }
 
 template <typename T, typename A>
-T& MyVector<T,A>::at (size_t n)
+T& MyVector<T,A>::at (size_type n)
 {
     if (n < 0 || this->sz <= n) throw out_of_range("Out of range");
     return this->elem[n];
 }
 
 template <typename T, typename A>
-const T& MyVector<T,A>::at (size_t n) const
+const T& MyVector<T,A>::at (size_type n) const
 {
     if (n < 0 || this->sz <= n) throw out_of_range("Out of range");
     return this->elem[n];
 }
 
 template <typename T, typename A>
-T& MyVector<T,A>::operator[] (size_t n)
+T& MyVector<T,A>::operator[] (size_type n)
 {
     return this->elem[n];
 }
 
 template <typename T, typename A>
-const T& MyVector<T,A>::operator[] (size_t n) const
+const T& MyVector<T,A>::operator[] (size_type n) const
 {
     return this->elem[n];
 }
+
+template <class T>
+void print(const MyVector<T> &src)
+{
+    for (auto i = 0; i < src.size(); ++i) {
+        cout << src[i] << " ";
+    }
+    cout << endl;
+}
+
 
 //end of class MyVector
 
